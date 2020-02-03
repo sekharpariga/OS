2e51ea4dea2f51a4afad8f03dbd1e739c2c28f44
Sekhar Pariga <sekharpariga@gmail.com>
2020-02-03 09:47:29 +0530

    updated gdt.cpp

--- gdt.cpp
+++ gdt.cpp
@@ -14,3 +14,18 @@ dataSegmentSelector(0,64*1024*1024, 0x92)
     asm volatile("lgdt (%0)": : "p" (((uint8_t *) i) +2));
 
 }
+
+globalDescriptorTable::~globalDescriptorTable()
+{}
+
+uint16_t globalDescriptorTable::DataSegmentselector()
+{
+    return (uint8_t *) &dataSegmentSelector - (uint8_t *) this;
+ }
+
+ uint16_t globalDescriptorTable::CodeSegmentSelector()
+ {
+     return (uint8_t *) &codeSegmentSelector - (uint8_t *) this;
+ }
+
+ globalDescriptorTable::Seg
