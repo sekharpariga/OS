90b451d239ec44664cadd03ca7990d2c2dc953f9
Sekhar Pariga <sekharpariga@gmail.com>
2020-02-04 18:18:13 +0530

    update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -4,9 +4,40 @@
 void printf(char* str)
 {
     uint16_t * videoMemory = (uint16_t *) 0xb8000;
+
+    static uint8_t x = 0, y = 0;
     
     for(int i = 0; str[i] != '\0'; ++i)
-        videoMemory[i] = (videoMemory[i] & 0xFF00 )| str[i] ;
+    {
+        if(str[i] == '\n')
+        {
+            y += 1;
+            x = 0;
+        }
+        if( y <= 80 & x <= 25)
+        {
+            videoMemory[i + (80*x) + y] = (videoMemory[i + (80*x) + y] & 0xFF00 )| str[i] ;
+        }
+        else
+        {
+            if(x == 25)
+            {
+                x = 0;
+                y += 1;
+            }
+            if( y == 255)
+            {
+                for(uint8_t x1 = 0; x1 <25; x1++)
+                    for(uint8_t y1 = 0; y1 < 80; y1++ )
+                    {
+                         videoMemory[(80*x1) + y1] = (videoMemory[ (80*x1) + y1] & 0xFF00 ) | ' ' ;
+                    }
+                    x = 0;
+                    y = 0;
+            }
+        }
+        
+    }
 }
 
 
@@ -24,6 +55,8 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(void* multiboot_structure, uint32_t  magicnumber)
 {
     printf("How are you Sekhar, Welcome to OS World\n");
+    printf("Thank you, my pleasure");
+    printf("Your welcome :)");
     globalDescriptorTable gdt;
-    while(1);       //infinate loop
+    while(1);       //infinite loop
 }
