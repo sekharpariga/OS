c31ac7a3148e43a507bc449edc2a64d23135625c
Sekhar Pariga <sekharpariga@gmail.com>
2020-02-15 14:37:21 +0530

    kernel hello, world module

---
+++ hello.c
@@ -0,0 +1,33 @@
+/*
+ * hello.c - The Hello, World Kernel Module
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/*
+ * hello_int - the init function, called when the module is loaded.
+ * return zero if successfully loaded, nonzero otherwise.
+ */
+
+static int hello_int(void)
+{
+	printk(KERNEL_ALART "I bear a charmed life. \n");
+}
+
+/*
+ * hello_exit - the exit function is called when the module is removed.
+ */
+
+static void hello_exit(void)
+{
+	printk(KERNEL_ALART "Out, out, breif candle!\n");
+}
+
+module_init(hello_int);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sekhar Pariga");
+MODULE_DESCRIPTION("A Hello, World Module");
